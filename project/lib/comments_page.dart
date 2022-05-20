834b788df53e3c73be4fab78d909a896cbe8a5ad
THE11th <vonatsiv1030@gmail.com>
2022-05-21 01:43:11 +0800

    feat: add comments fetch

--- project/lib/comments_page.dart
+++ project/lib/comments_page.dart
@@ -9,13 +9,31 @@ import 'models/comment_model.dart';
 import 'feed_page.dart';
 
 class Comments extends StatefulWidget {
-  const Comments({Key? key}) : super(key: key);
+  const Comments({Key? key, required this.id}) : super(key: key);
+
+  final String id;
 
   @override
   State<Comments> createState() => _CommentsState();
 }
 
 class _CommentsState extends State<Comments> {
+  late Future<List<Comment>> comments;
+
+  LocalStorage storage = LocalStorage('project');
+
+  //comment controller
+  final TextEditingController _commentController = TextEditingController();
+
+  DBHelper db = DBHelper();
+
+  @override
+  void initState() {
+    super.initState();
+    comments = db.getComments(widget.id);
+    print(widget.id);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
