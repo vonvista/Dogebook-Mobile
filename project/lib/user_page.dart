40b64e38e08b31b19f5b5d8ab7fb9e14c6e0d50f
THE11th <vonatsiv1030@gmail.com>
2022-05-21 01:12:44 +0800

    feat: user page initialization

---
+++ project/lib/user_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'db_helper.dart';
+
+import 'package:localstorage/localstorage.dart';
+
+import 'models/user_model.dart';
+import 'models/post_model.dart';
+
+import 'login_page.dart';
+import 'profile_page.dart';
+
+class UserPage extends StatefulWidget {
+  const UserPage({Key? key}) : super(key: key);
+
+  @override
+  State<UserPage> createState() => _UserPageState();
+}
+
+class _UserPageState extends State<UserPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
