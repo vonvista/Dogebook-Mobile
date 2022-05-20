1186e434752fca2486f4b3ada6fdd8c3aa562f9b
THE11th <vonatsiv1030@gmail.com>
2022-05-21 01:24:04 +0800

    feat: initialize friend page

---
+++ project/lib/friend_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'db_helper.dart';
+
+import 'package:localstorage/localstorage.dart';
+
+import 'models/user_model.dart';
+import 'models/post_model.dart';
+
+import 'feed_page.dart';
+import 'profile_page.dart';
+
+class FriendPage extends StatefulWidget {
+  const FriendPage({Key? key}) : super(key: key);
+
+  @override
+  State<FriendPage> createState() => _FriendPageState();
+}
+
+class _FriendPageState extends State<FriendPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
