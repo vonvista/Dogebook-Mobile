c4869d4c9e7eebf7eedf45a5bb3b6f9458c26500
THE11th <vonatsiv1030@gmail.com>
2022-05-21 00:55:12 +0800

    feat: initialize search page

---
+++ project/lib/search_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'db_helper.dart';
+
+import 'models/user_model.dart';
+
+import 'package:localstorage/localstorage.dart';
+import 'package:flutter_profile_picture/flutter_profile_picture.dart';
+
+class SearchPage extends StatefulWidget {
+  const SearchPage({Key? key}) : super(key: key);
+
+  @override
+  State<SearchPage> createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
