ed5d7b9ce6baadc81a2ca461858f75889bdef75f
THE11th <vonatsiv1030@gmail.com>
2022-05-20 23:38:27 +0800

    feat: initialize feed page

---
+++ project/lib/feed_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'db_helper.dart';
+
+import 'package:localstorage/localstorage.dart';
+
+import 'models/post_model.dart';
+import 'models/user_model.dart';
+
+class Feed extends StatefulWidget {
+  const Feed({Key? key}) : super(key: key);
+
+  @override
+  State<Feed> createState() => _FeedState();
+}
+
+class _FeedState extends State<Feed> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
