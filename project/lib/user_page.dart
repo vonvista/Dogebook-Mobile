e60f3e3907741be42a97c83cfb17508662fd4675
THE11th <vonatsiv1030@gmail.com>
2022-05-21 01:13:20 +0800

    feat: create list tile for user

--- project/lib/user_page.dart
+++ project/lib/user_page.dart
@@ -17,6 +17,49 @@ class UserPage extends StatefulWidget {
 }
 
 class _UserPageState extends State<UserPage> {
+  final LocalStorage storage = LocalStorage('project');
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  //create list tile for user
+  Widget _userListTile(String id, String name, String username) {
+    return ListTile(
+      leading: Hero(
+        tag: 'image_$id',
+        child: const Icon(
+          Icons.person,
+          color: Colors.blue,
+        ),
+      ),
+      title: Hero(
+        tag: "text_$id",
+        child: Material(
+          color: Colors.transparent,
+          child: Text(
+            name,
+            style: TextStyle(fontSize: 16, color: Colors.black, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+      subtitle: Text(username),
+      onTap:
+          //navigate to profile page
+          () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => ProfilePage(
+              userId: id,
+            ),
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
