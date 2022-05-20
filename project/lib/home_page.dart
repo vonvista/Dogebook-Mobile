8da4e387e1309baabe73b808abf85da658393d8f
THE11th <vonatsiv1030@gmail.com>
2022-05-20 23:30:15 +0800

    feat: initialize home page

---
+++ project/lib/home_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'models/user_model.dart';
+
+import 'package:localstorage/localstorage.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
