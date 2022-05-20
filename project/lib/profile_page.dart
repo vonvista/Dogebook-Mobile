0ef392882b153ab5c096fa08ce15dc21d23048ef
THE11th <vonatsiv1030@gmail.com>
2022-05-21 01:05:36 +0800

    feat: initialize profile page

---
+++ project/lib/profile_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'db_helper.dart';
+
+import 'package:localstorage/localstorage.dart';
+import 'package:flutter_profile_picture/flutter_profile_picture.dart';
+
+import 'models/user_model.dart';
+import 'models/post_model.dart';
+
+import 'feed_page.dart';
+
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({Key? key}) : super(key: key);
+
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
