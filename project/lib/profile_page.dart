7a921f2e47189a233026de854e7e49babf6baa00
THE11th <vonatsiv1030@gmail.com>
2022-05-21 01:09:25 +0800

    feat: create profile page UI

--- project/lib/profile_page.dart
+++ project/lib/profile_page.dart
@@ -10,15 +10,119 @@ import 'models/post_model.dart';
 import 'feed_page.dart';
 
 class ProfilePage extends StatefulWidget {
-  const ProfilePage({Key? key}) : super(key: key);
+  const ProfilePage({
+    Key? key,
+    required this.userId,
+  }) : super(key: key);
+
+  final String userId;
 
   @override
   State<ProfilePage> createState() => _ProfilePageState();
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  late Future<User> user;
+
+  DBHelper db = DBHelper();
+
+  final LocalStorage storage = LocalStorage('project');
+
+  @override
+  void initState() {
+    super.initState();
+    user = db.getUser(id: widget.userId);
+    print(storage.getItem('friends').contains(widget.userId));
+  }
+
+  //create edit profile button
+  Widget _editProfileButton() {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      //if profile is user, show edit profile button, else show add friend button
+
+      child: widget.userId == storage.getItem('_id')
+          ? ElevatedButton(
+              child: Text('Edit Profile'),
+              onPressed: () {
+                print("Saem");
+              },
+            )
+          : storage.getItem('friends').contains(widget.userId)
+              ? ElevatedButton(
+                  child: Text('Friends'),
+                  onPressed: () {},
+                )
+              : ElevatedButton(
+                  child: Text('Send friend request'),
+                  onPressed: () {
+                    //NOTE: Put here send friend request logic
+                  },
+                ),
+    );
+  }
+
+  //build profile page with user info
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(''),
+        foregroundColor: Colors.black,
+        backgroundColor: Colors.white,
+      ),
+      body: Center(
+        child: FutureBuilder<User>(
+          future: user,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return ListView(
+                children: [
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Center(
+                    child: Hero(
+                      tag: "image_${snapshot.data!.id}",
+                      child: ProfilePicture(
+                        name: snapshot.data!.firstName + ' ' + snapshot.data!.lastName,
+                        radius: 31,
+                        fontsize: 21,
+                      ),
+                    ),
+                  ),
+                  Center(
+                    child: Hero(
+                      tag: "text_${snapshot.data!.id}",
+                      child: Material(
+                        child: Text(
+                          snapshot.data!.firstName + " " + snapshot.data!.lastName,
+                          style: TextStyle(
+                            fontSize: 30,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Center(
+                    child: Text(
+                      snapshot.data!.email,
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                  _editProfileButton(),
+                  Feed(mode: "widget", userId: widget.userId),
+                ],
+              );
+            } else if (snapshot.hasError) {
+              return Text("${snapshot.error}");
+            }
+            return CircularProgressIndicator();
+          },
+        ),
+      ),
+    );
   }
 }
