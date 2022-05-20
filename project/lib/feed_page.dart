f7da9d837cafb70d15031b6790338df56ecb81f2
THE11th <vonatsiv1030@gmail.com>
2022-05-21 00:07:10 +0800

    feat: add listview for posts and api fetch

--- project/lib/feed_page.dart
+++ project/lib/feed_page.dart
@@ -22,14 +22,77 @@ class Feed extends StatefulWidget {
 }
 
 class _FeedState extends State<Feed> {
+  //variable for post privacy
+  String privacy = 'public';
+  String viewPostsOf = 'public';
+
   DBHelper db = DBHelper();
 
   late Future<List<Post>> posts = Future.value([]);
   final LocalStorage storage = LocalStorage('project');
 
+  int limit = 0;
+  bool hasMorePost = true;
+
+  //post controller
+  final TextEditingController _postController = TextEditingController();
+  final _postScrollController = ScrollController();
+
   @override
-  Widget build(BuildContext context) {
-    return Container();
+  void initState() {
+    super.initState();
+    limit = db.limit;
+    fetchNextPosts();
+    // db.getPublicPostsLim("628495b811dbd074a7d039e5");
+
+    ///NOTE: This is replaced by a button due to complication with nested scroll view
+
+    // _postScrollController.addListener(() {
+
+    //   if (_postScrollController.position.pixels ==
+    //       _postScrollController.position.maxScrollExtent) {
+    //     //get more posts
+    //     print("HERE");
+    //     fetchNextPosts();
+    //   }
+    // });
+  }
+
+  @override
+  void dispose() {
+    _postController.dispose();
+    super.dispose();
+  }
+
+  void fetchNextPosts() async {
+    //get length of future post
+    int postLength = await posts.then((value) => value.length);
+
+    String lastpostid;
+
+    if (postLength > 0) {
+      Post lastpost = await posts.then((value) => value.last);
+      //get last post id
+      lastpostid = lastpost.id;
+    } else {
+      lastpostid = "";
+    }
+    //return next posts
+    dynamic newPosts;
+    print("FETCH!");
+    if (widget.mode == "normal") {
+      newPosts = db.getPublicPostsLim(lastpostid);
+    } else if (widget.mode == "widget") {
+      newPosts = db.getUserPostsLim(widget.userId, lastpostid);
+    }
+    // dynamic newPosts = db.getPublicPostsLim(lastpostid);
+    //append new posts and old posts
+    int newPostsLength = await newPosts.then((value) => value.length);
+    newPostsLength < limit ? hasMorePost = false : hasMorePost = true;
+    print(hasMorePost);
+    setState(() {
+      posts = Future.wait([posts, newPosts as Future<List<Post>>]).then((value) => value.expand((x) => x).toList());
+    });
   }
 
   //create button for add post
@@ -132,5 +195,60 @@ class _FeedState extends State<Feed> {
         ),
       ),
     );
+  }
+
+  //create list of posts using future builder
+  Widget _postsList() {
+    return FutureBuilder<List<Post>>(
+      future: posts,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          //use notificationlistener to detect if scroll has reached end of list
+          return ListView.builder(
+            // controller: _postScrollController,
+            shrinkWrap: true,
+            physics: ClampingScrollPhysics(),
+            itemCount: snapshot.data!.length + 1,
+            itemBuilder: (context, index) {
+              if (index < snapshot.data!.length) {
+                return _postCard(
+                  snapshot.data![index].id,
+                  snapshot.data![index].name,
+                  snapshot.data![index].createdAt,
+                  snapshot.data![index].content,
+                  snapshot.data![index].privacy,
+                  snapshot.data![index].userId,
+                );
+              } else {
+                return Center(
+                  child: hasMorePost
+                      ? //created button to load more posts
+                      ElevatedButton(
+                          child: Text('Load More'),
+                          onPressed: () {
+                            fetchNextPosts();
+                          },
+                        )
+                      : Text('No more posts'),
+                );
+              }
+            },
+          );
+        } else if (snapshot.hasError) {
+          return Text("${snapshot.error}");
+        } else if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        return Container();
+        // return Center(child: CircularProgressIndicator());
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
