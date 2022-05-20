71a7aa6a46932418f65778bdda56ea621daeb9be
THE11th <vonatsiv1030@gmail.com>
2022-05-20 23:40:52 +0800

    feat: create post bar

--- project/lib/feed_page.dart
+++ project/lib/feed_page.dart
@@ -7,15 +7,85 @@ import 'models/post_model.dart';
 import 'models/user_model.dart';
 
 class Feed extends StatefulWidget {
-  const Feed({Key? key}) : super(key: key);
+  const Feed({
+    Key? key,
+    this.userId = "",
+    this.mode = "normal",
+  }) : super(key: key);
+
+  final String mode;
+  final String userId;
 
   @override
   State<Feed> createState() => _FeedState();
 }
 
 class _FeedState extends State<Feed> {
+  DBHelper db = DBHelper();
+
+  late Future<List<Post>> posts = Future.value([]);
+  final LocalStorage storage = LocalStorage('project');
+
   @override
   Widget build(BuildContext context) {
     return Container();
+  }
+
+  //create button for add post
+  Widget _postBar() {
+    return Row(
+      children: [
+        Expanded(
+          child: ElevatedButton(
+            //text and icon
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const <Widget>[
+                Icon(Icons.add),
+                Text('Add Post'),
+              ],
+            ),
+            //on press placeholder
+            onPressed: () {},
+          ),
+        ),
+        const SizedBox(width: 10),
+        // Text("Show posts of: "),
+        // //create dropdown selector for viewing public or friends post
+        // DropdownButton<String>(
+        //   value: privacy,
+        //   icon: Icon(Icons.arrow_drop_down),
+        //   iconSize: 24,
+        //   elevation: 16,
+        //   style: TextStyle(color: Colors.deepPurple),
+        //   underline: Container(
+        //     height: 2,
+        //     color: Colors.deepPurpleAccent,
+        //   ),
+        //   onChanged: (String? newValue) {
+        //     setState(() {
+        //       privacy = newValue!;
+        //     });
+        //   },
+        //   //items with text and icon for public and friends
+        //   items: <String>['public', 'friends']
+        //       .map<DropdownMenuItem<String>>((String value) {
+        //     return DropdownMenuItem<String>(
+        //       value: value,
+        //       child: Row(
+        //         children: <Widget>[
+        //           Icon(
+        //             value == 'public' ? Icons.public : Icons.people,
+        //             color: Colors.deepPurple,
+        //           ),
+        //           const SizedBox(width: 10),
+        //           Text(value),
+        //         ],
+        //       ),
+        //     );
+        //   }).toList(),
+        //),
+      ],
+    );
   }
 }
