f3f1e33a8f07a7ad17f7c0ed7c2f8e8c68ee071c
THE11th <vonatsiv1030@gmail.com>
2022-05-20 23:08:22 +0800

    feat: add login page UI

--- project/lib/login_page.dart
+++ project/lib/login_page.dart
@@ -12,8 +12,122 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  //create email and password controllers
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  //create a global key for form validation
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  final LocalStorage storage = LocalStorage('project');
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  //create email and password text fields with validators
+  Widget _emailField() {
+    return TextFormField(
+      controller: _emailController,
+      decoration: const InputDecoration(
+        labelText: 'Email',
+        hintText: 'Enter your email',
+        icon: Icon(Icons.email),
+      ),
+      keyboardType: TextInputType.emailAddress,
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Please enter your email';
+        }
+        return null;
+      },
+    );
+  }
+
+  Widget _passwordField() {
+    return TextFormField(
+      controller: _passwordController,
+      decoration: const InputDecoration(
+        labelText: 'Password',
+        hintText: 'Enter your password',
+        icon: Icon(Icons.lock),
+      ),
+      obscureText: true,
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Please enter your email';
+        }
+        return null;
+      },
+    );
+  }
+
+  //reusable widget for button
+  Widget _buildButton(String text, Function() onPressed) {
+    return ElevatedButton(
+      //set button size
+      child: Text(text),
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        //set button color
+        primary: Colors.blue,
+        minimumSize: const Size.fromHeight(50),
+      ),
+    );
+  }
+
+  Widget _buildSignUp() {
+    return Row(
+      //center the buttons
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text('Don\'t have an account?'),
+        const SizedBox(width: 10),
+        //create a link to sign up page that is clickable
+        ElevatedButton(
+          child: const Text(
+            'Sign Up',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+              //set the button to no background
+              ),
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.all(30.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'Log in',
+                //text size to 40
+                style: TextStyle(fontSize: 40),
+              ),
+              const SizedBox(height: 30),
+              _emailField(),
+              const SizedBox(height: 10),
+              _passwordField(),
+              const SizedBox(height: 10),
+              _buildButton('Login', () => {}),
+              const SizedBox(height: 10),
+              _buildSignUp(),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
