870701ef425df8a23d01e8335138817881ef2348
THE11th <vonatsiv1030@gmail.com>
2022-05-21 11:04:49 +0800

    style: add colors

---
+++ project/lib/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  Color deg1 = const Color(0xFF242F9B);
+  Color deg2dark = const Color(0xFF333C8F);
+  Color deg2 = const Color(0xFF646FD4);
+  Color deg3 = const Color(0xFF9BA3EB);
+  Color deg4 = const Color(0xFFDBDFFD);
+  Color deg5 = const Color(0xFFF2F2F2);
+}
